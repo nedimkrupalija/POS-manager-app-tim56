8ff71b5f8455ee797f2cef137144097efd276eb1
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-03-23 15:00:09 +0100

    Added POST request submission for login route

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import './Login.css';
+
+import person_icon from '../../assets/person.png';
+import pass_icon from '../../assets/password.png';
+
+const Login = () => {
+    const handleLogin = () => {
+        const usernameOrPhone = document.getElementById('usernameOrPhone').value;
+        const password = document.getElementById('password').value;
+
+        let requestBody;
+        if (isNaN(usernameOrPhone)) { 
+            requestBody = { username: usernameOrPhone, password: password, role:"admin" };
+            console.log(requestBody)
+        } else {
+            requestBody = { phoneNumber: usernameOrPhone, password: password, role:"admin" };
+        }
+
+        fetch('http://localhost:3000/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        })
+        .then(data => {
+            console.log(data); 
+        })
+        .catch(error => {
+            console.error('Error:', error); 
+            alert('Error occurred while logging in. Please try again later.');
+        });
+    };
+
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">Log in</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={person_icon} alt=""/>
+                    <input id="usernameOrPhone" type="text" placeholder='Username or phone number'/>
+                </div> 
+                <div className="input">
+                    <img src={pass_icon} alt=""/>
+                    <input id="password" type="password" placeholder='Password'/>
+                </div> 
+            </div>
+            <div className="submit-container">
+                <div className="submit" onClick={handleLogin}>Login</div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
