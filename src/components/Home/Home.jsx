10711b2c42ca84e52351db834eb47d3d3b464daa
akrslak1 <92301603+akrslak1@users.noreply.github.com>
2024-03-25 17:47:47 +0100

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Home.css'; // Uvezite CSS datoteku
 
-const Home = ({ onLogout }) => {
+const Home = ({ onLogout, onClientApp }) => {
     return (
-      <div className="container">
         <div className="button-container">
-          <button onClick={onLogout}>Odjava</button>
+          <button onClick={() => window.location.href = "https://pos-employees-app-tim56.vercel.app"}>
+    Client app
+    </button>
+   <button onClick={onLogout}>Odjava</button>
         </div>
-      </div>
+     
     );
   };
   
