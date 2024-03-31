dd9c35215b074bbf974035ea034d4c14408e8993
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-03-31 17:49:43 +0200

    Minor design changes

--- src/components/CRUDUsers/CRUDUsers.jsx
+++ src/components/CRUDUsers/CRUDUsers.jsx
@@ -1,83 +1,93 @@
 import React, { useState } from 'react';
 import './CRUDUsers.css';
+import edit_icon from '../../assets/edit.png'
+import delete_icon from '../../assets/delete.png'
 
 const CRUDUsers = () => {
+    const [tableVisible, settableVisible] = useState(true);
     return (
         <>
-        <div className='list'>
-            <h2>USERS</h2>
-            <button className='buttons1'>CREATE NEW</button>
-            <div className='table'>
-                <table border="1">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Username</th>
-                            <th>Phone Number</th>
-                            <th>Password</th>
-                            <th>Role</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>tajra</td>
-                            <td>060111333</td>
-                            <td>tajra</td>
-                            <td>Admin</td>
-                            <td>
-                                <button className='buttons'>EDIT</button>
-                                <button className='buttons'>DELETE</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>amina</td>
-                            <td>060111333</td>
-                            <td>amina</td>
-                            <td>Admin</td>
-                            <td>
-                                <button className='buttons'>EDIT</button>
-                                <button className='buttons'>DELETE</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div className='list'>
+                <h2 className='users-title'>{tableVisible ? "USERS" : "CREATE NEW USER"}</h2>
+                <div className="buttons-container">
+                    <button disabled = {tableVisible} className={tableVisible ? 'buttons' : 'buttons1'} onClick={() => { settableVisible(true) }}>LIST USERS</button>
+                    <button disabled = {!tableVisible} className={tableVisible ? 'buttons1' : 'buttons'} onClick={() => { settableVisible(false) }}>CREATE NEW</button>
+                </div>
+                {tableVisible && <div className='table'>
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Phone Number</th>
+                                <th>Password</th>
+                                <th>Role</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>tajra</td>
+                                <td>060111333</td>
+                                <td>tajra</td>
+                                <td>Admin</td>
+                                <td>
+                                    <div className='actions-containter'>
+                                        <img src={edit_icon} alt="Edit" className='edit-icon' />
+                                        <img src={delete_icon} alt="Delete" className='delete-icon' />
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>amina</td>
+                                <td>060111333</td>
+                                <td>amina</td>
+                                <td>Admin</td>
+                                <td>
+                                    <div className='actions-containter'>
+                                        <img src={edit_icon} alt="Edit" className='edit-icon' />
+                                        <img src={delete_icon} alt="Delete" className='delete-icon' />
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                }
             </div>
-        </div>
 
-        <div className='create'>
-            <h2>CREATE NEW USER</h2>
-            <button className='buttons1'>LIST USERS</button>
-            <div className='createFields'>
-                <label htmlFor="username" className='fields'>Username:</label>
-                <input type="text" id="usernameCreate" className="username-input" placeholder="Username" />
-                <br />
-                <label htmlFor="password" className='fields'>Password:</label>
-                <input type="text" id="passwordCreate" className="password-input" placeholder="Password" />
-                <br />
-                <label htmlFor="phone" className='fields'>Phone Number:</label>
-                <input type="text" id="phoneCreate" className="phone-input" placeholder="Phone Number" />
+            {!tableVisible && <div className='create'>
+                <div className='createFields'>
+                    <label htmlFor="username" className='fields'>Username:</label>
+                    <input type="text" id="usernameCreate" className="username-input" placeholder="Username" />
+                    <br />
+                    <label htmlFor="password" className='fields'>Password:</label>
+                    <input type="text" id="passwordCreate" className="password-input" placeholder="Password" />
+                    <br />
+                    <label htmlFor="phone" className='fields'>Phone Number:</label>
+                    <input type="text" id="phoneCreate" className="phone-input" placeholder="Phone Number" />
+                </div>
+                <button className='button2'>CREATE</button>
             </div>
-            <button className='button2'>CREATE</button>
-        </div>
+            }
 
-        <div className='edit'>
-        <h2>EDIT USER</h2>
-            <button className='buttons1'>LIST USERS</button>
-            <div className='createFields'>
-                <label htmlFor="username" className='fields'>Username:</label>
-                <input type="text" id="usernameEdit" className="username-input" placeholder="Username" />
-                <br />
-                <label htmlFor="password" className='fields'>Password:</label>
-                <input type="text" id="passwordEdit" className="password-input" placeholder="Password" />
-                <br />
-                <label htmlFor="phone" className='fields'>Phone Number:</label>
-                <input type="text" id="phoneEdit" className="phone-input" placeholder="Phone Number" />
-            </div>
-            <button className='button2'>EDIT</button>
-        </div>
+            {/*  <div className='edit'>
+                <h2>EDIT USER</h2>
+                <button className='buttons1'>LIST USERS</button>
+                <div className='createFields'>
+                    <label htmlFor="username" className='fields'>Username:</label>
+                    <input type="text" id="usernameEdit" className="username-input" placeholder="Username" />
+                    <br />
+                    <label htmlFor="password" className='fields'>Password:</label>
+                    <input type="text" id="passwordEdit" className="password-input" placeholder="Password" />
+                    <br />
+                    <label htmlFor="phone" className='fields'>Phone Number:</label>
+                    <input type="text" id="phoneEdit" className="phone-input" placeholder="Phone Number" />
+                </div>
+                <button className='button2'>EDIT</button>
+        </div> */}
 
         </>
     );
