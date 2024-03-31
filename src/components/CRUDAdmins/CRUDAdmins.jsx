f55b5f8d89b6206ca2436bac0804a2ce2e490f67
azunic3 <azunic3@etf.unsa.ba>
2024-03-31 15:26:30 -0700

    fetching data

--- src/components/CRUDAdmins/CRUDAdmins.jsx
+++ src/components/CRUDAdmins/CRUDAdmins.jsx
@@ -1,17 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CRUDAdmins.css';
 import edit_icon from '../../assets/edit.png'
 import delete_icon from '../../assets/delete.png'
+import {Link} from "react-router-dom"
+
 
 const CRUDAdmins = () => {
     const [tableVisible, settableVisible] = useState(true);
+
+const fetchAdmins = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/admin/administators', {
+            headers: {
+                'Authorization': `${token}`
+            }
+        });
+        if (!response.ok) {
+            throw new Error('Error fetching administators');
+        }
+        const data = await response.json();
+        console.log(data)
+    } catch (error) {
+        console.error(error);
+    }
+};
     return (
         <>
             <div className='list'>
-                <h2 className='users-title'>{tableVisible ? "ADMINS" : "CREATE NEW ADMIN"}</h2>
+                <h2 className='users-title'>{tableVisible ? "ADMINS" : "Create new admin"}</h2>
                 <div className="buttons-container">
-                    <button disabled={tableVisible} className={tableVisible ? 'buttons' : 'buttons1'} onClick={() => { settableVisible(true); fetchUsers(); }}>LIST ADMINS</button>
-                    <button disabled={!tableVisible} className={tableVisible ? 'buttons1' : 'buttons'} onClick={() => { settableVisible(false) }}>CREATE NEW</button>
+                    <button disabled={tableVisible} className={tableVisible ? 'buttons' : 'buttons1'} onClick={() => { settableVisible(true); fetchAdmins(); }}>List admins</button>
+                    <button disabled={!tableVisible} className={tableVisible ? 'buttons1' : 'buttons'} onClick={() => { settableVisible(false) }}>Create new</button>
                 </div>
                 {tableVisible && <div className='table'>
                     <table border="1">
@@ -26,32 +45,6 @@ const CRUDAdmins = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>admin1</td>
-                                <td>060111333</td>
-                                <td>admin1</td>
-                                <td>Admin</td>
-                                <td>
-                                    <div className='actions-containter'>
-                                        <img src={edit_icon} alt="Edit" className='edit-icon' />
-                                        <img src={delete_icon} alt="Delete" className='delete-icon' />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>admin2</td>
-                                <td>060111333</td>
-                                <td>admin2</td>
-                                <td>Admin</td>
-                                <td>
-                                    <div className='actions-containter'>
-                                        <img src={edit_icon} alt="Edit" className='edit-icon' />
-                                        <img src={delete_icon} alt="Delete" className='delete-icon' />
-                                    </div>
-                                </td>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
@@ -64,24 +57,24 @@ const CRUDAdmins = () => {
                     <input type="text" id="usernameCreate" className="username-input" placeholder="Username" />
                     <br />
                     <label htmlFor="password" className='fields'>Password:</label>
-                    <input type="text" id="passwordCreate" className="password-input" placeholder="Password" />
+                    <input type="password" id="passwordCreate" className="password-input" placeholder="Password" />
                     <br />
                     <label htmlFor="phone" className='fields'>Phone Number:</label>
                     <input type="text" id="phoneCreate" className="phone-input" placeholder="Phone Number" />
                 </div>
-                <button className='button2'>CREATE</button>
+                <button className='button2'>Create</button>
             </div>
             }
 
             {/*  <div className='edit'>
-                <h2>EDIT USER</h2>
-                <button className='buttons1'>LIST USERS</button>
+                <h2>Edit admin</h2>
+                <button className='buttons1'>List admins</button>
                 <div className='createFields'>
                     <label htmlFor="username" className='fields'>Username:</label>
                     <input type="text" id="usernameEdit" className="username-input" placeholder="Username" />
                     <br />
                     <label htmlFor="password" className='fields'>Password:</label>
-                    <input type="text" id="passwordEdit" className="password-input" placeholder="Password" />
+                    <input type="password" id="passwordEdit" className="password-input" placeholder="Password" />
                     <br />
                     <label htmlFor="phone" className='fields'>Phone Number:</label>
                     <input type="text" id="phoneEdit" className="phone-input" placeholder="Phone Number" />
