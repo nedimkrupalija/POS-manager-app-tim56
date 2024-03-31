8e131e2b6abedf277f4c8ed2d225138f15ac8fad
ebronja1 <ebronja1@etf.unsa.ba>
2024-03-31 17:17:08 +0200

    done with hardcoded data

--- src/components/Storage/Storage.jsx
+++ src/components/Storage/Storage.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import './LocationDetails.css'; // Import CSS file
+import './Storage.css'; // Import CSS file
 
-const Storage = (storage) => {
-  return (
-    <div>
-      <h2>Storage: {storage.storageId}</h2>
-      <h2>Storage status: {storage.storageStatus}</h2>
-      <ul>
-        {storage.products.map(product => (
-          <li key={product.id}>
-            {product.name}: {product.quantity}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+const storage = {
+  storageId: 1,
+  location: 'Location A',
+  items: [
+    { name: 'Item 1', quantity: 10 },
+    { name: 'Item 2', quantity: 20 },
+    { name: 'Item 3', quantity: 15 },
+    { name: 'Item 4', quantity: 15 },
+    { name: 'Item 5', quantity: 15 },
+    { name: 'Item 6', quantity: 15 },
+    { name: 'Item 7', quantity: 15 },
+  ],
 };
+
+const Storage = () => {
+  return (
+    <div className='storage-container'>
+      <h2>Storage location: {storage.location}</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Item Name</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {storage.items.map((product, index) => (
+            <tr key={index}>
+              <td>{product.name}</td>
+              <td>{product.quantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )}
 
 export default Storage;
