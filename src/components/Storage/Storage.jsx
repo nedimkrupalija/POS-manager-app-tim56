4c7711d2cdb868dc7e35b433ddde4b2b3d41cce9
ebronja1 <ebronja1@etf.unsa.ba>
2024-04-01 21:32:19 +0200

    finished but css could be changed at will

--- src/components/Storage/Storage.jsx
+++ src/components/Storage/Storage.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Storage.css'; // Import CSS file
 
-const storage = {
-  storageId: 1,
-  location: 'Location A',
-  items: [
-    { name: 'Item 1', quantity: 10 },
-    { name: 'Item 2', quantity: 20 },
-    { name: 'Item 3', quantity: 15 },
-    { name: 'Item 4', quantity: 15 },
-    { name: 'Item 5', quantity: 15 },
-    { name: 'Item 6', quantity: 15 },
-    { name: 'Item 7', quantity: 15 },
-  ],
-};
+const Storage = ({id}) => {
+  const [storage, setStorage] = useState({});
+  const [storageId, setStorageId] = useState();
+  const [errorMessage, setErrorMessage] = useState('');
 
-const Storage = () => {
+  useEffect(() => {
+    setStorageId(storageId);
+    fetchStorage();
+  }, [id]);
+
+  const fetchStorage = async () => {
+    try {
+      const headers = {
+        'Authorization': `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJ1c2VybmFtZSI6ImFtaW5hIiwiaWF0IjoxNzExOTM0NDcwLCJleHAiOjE3MTE5MzYyNzB9.xx2wj0WaeTGd4Su7XtfR2bUBIrOzWHyMCJp433Ea1xw`
+      };
+      const response = await fetch('http://localhost:3000/storage/'+id , { headers });
+      const data = await response.json();
+      setStorage(data);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
   return (
     <div className='storage-container'>
-      <h2>Storage location: {storage.location}</h2>
+      <h2>Storage location: {storage.Location && storage.Location.name}</h2>
+      <h2>Storage status: {storage.status}</h2>
       <table>
         <thead>
           <tr>
@@ -27,15 +36,18 @@ const Storage = () => {
           </tr>
         </thead>
         <tbody>
-          {storage.items.map((product, index) => (
+          {storage.Items && storage.Items.map((product, index) => (
             <tr key={index}>
               <td>{product.name}</td>
-              <td>{product.quantity}</td>
+              <td>{product.StorageItem.quantity}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      {errorMessage && <p>Error: {errorMessage}</p>}
     </div>
-  )}
+  );
+};
 
 export default Storage;
+
