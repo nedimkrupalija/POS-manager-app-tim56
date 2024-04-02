b69a34278653fa584ac11d285a1913180c02bf45
akrslak1 <92301603+akrslak1@users.noreply.github.com>
2024-04-02 22:06:51 +0200

    Update CRUDLocation test

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,26 @@ import Home from './components/Home/Home.jsx'
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import CRUDLocations from './components/CRUDLocation/CRUDLocation.jsx';
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  /*const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const jwtToken = Cookies.get('jwt');
     setIsLoggedIn(!!jwtToken);
-  }, []);
+  }, []);*/
 
   return (
-      <Router>
+      /*<Router>
         <Routes>
           <Route path="/" element={ isLoggedIn ? <Home /> : <Login /> }></Route>
+        </Routes>
+      </Router>*/
+      <Router>
+        <Routes>
+          <Route path="/" element={<CRUDLocations />} />
         </Routes>
       </Router>
   )
