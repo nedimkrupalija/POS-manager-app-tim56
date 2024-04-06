61327e5d4280f495d7a8f3973c05ab3075b4ca3a
azunic3 <azunic3@etf.unsa.ba>
2024-04-06 15:57:10 -0700

    list and create form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import Login from './components/Login/Login.jsx'
 import Home from './components/Home/Home.jsx'
 import React, { useState, useEffect } from 'react';
-import Orders from './components/Orders/Orders.jsx';
 import Cookies from 'js-cookie';
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
-import CRUDLocations from './components/CRUDLocation/CRUDLocation.jsx';
 
-import CRUDAdmins from './components/CRUDAdmins/CRUDAdmins.jsx';
+import CRUDTablesStations from './components/CRUDTablesStations/CRUDTablesStations.jsx';
 
-import CRUDUsers from './components/CRUDUsers/CRUDUsers.jsx';
-
-import CRUDItems from './components/CRUDItems/CRUDItems.jsx';
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root'); 
@@ -25,7 +20,13 @@ function App() {
     setIsLoggedIn(!!jwtToken);
   }, []);
   return (
-    
+    <Router>
+    <Routes>
+    <Route path="/" element={<CRUDTablesStations />} />
+    </Routes>
+      </Router>
+
+    /*
       <Router basename='/'>
         <Routes>
           <Route path="/items" element={isLoggedIn?  <CRUDItems /> :<Login/> }></Route>
@@ -36,7 +37,7 @@ function App() {
           <Route path="/locations" element={isLoggedIn?  <CRUDLocations /> :<Login/> }/>
         </Routes>
       </Router>
-    
+    */
   )
 }
 export default App
---
+++ src/components/CRUDTablesStations/CRUDTablesStations.jsx
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from 'react';
+import './CRUDTablesStations.css';
+import close_icon from '../../assets/close.png'
+import info_icon from '../../assets/info.png'
+import Cookies from 'js-cookie';
+import Home from '../Home/Home';
+const CRUDTablesStations = () => {
+
+  const [stations, setStation] = useState([]);
+  const [tableVisible, settableVisible] = useState(true);
+  const [infoMessage, setInfoMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const token =()=>{
+   return Cookies.get("jwt");
+} 
+  useEffect(() => {
+    fetchTablesStations();
+}, []);
+
+  const fetchTablesStations = async () => {
+    try {
+        //const token=Cookies.get('jwt');
+        const headers = {
+            'Authorization': token()
+        };
+        const data = await fetchData('GET', 'https://pos-app-backend-tim56.onrender.com/admin/administrators', null, headers);
+        setStation(data);
+    } catch (error) {
+        console.error(error);
+    }
+  };
+
+//                                      CREATE
+  const createTablesStations = async () => {
+    const number = document.getElementById('numberCreate').value;
+
+    try {
+        if (number == '') {
+            setErrorMessage('All fields must be filled!')
+            setInfoMessage('')
+        }
+        else{
+        const requestData = { number};
+        const headers = {
+            'Authorization': token()
+        };
+        //link zamijenit
+        await fetchData('POST', 'https://pos-app-backend-tim56.onrender.com/admin/administrators', requestData, headers);
+        setInfoMessage('Table/Station created')
+                document.getElementById('numberCreate').value = ''
+                setErrorMessage('')   
+        fetchTablesStations(); 
+    }} catch (error) {
+        console.error(error);
+    }
+};
+
+
+
+const fetchData = async (method, url, requestData = null, headers = {}) => {
+    try {
+        const options = {
+            method: method,
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            }
+        };
+
+        if (requestData) {
+            options.body = JSON.stringify(requestData);
+        }
+
+        const response = await fetch(url, options);
+        const extendedToken=response.headers.get('Authorization');
+        console.log(extendedToken);
+        if(extendedToken){
+            Cookies.set(jwt,extendedToken,{expires:1/48});
+     
+        }
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || 'Error fetching data');
+        }
+   
+        return data;
+    } catch (error) {
+        throw new Error(error.message || 'Error fetching data');
+    }
+};
+
+
+
+return (
+    
+    <>
+    <div className='list'>
+        <h2 className='users-title'>{tableVisible ? "Table/Station" : "Create new table/station"}</h2>
+        <div className="buttons-container">
+            <button disabled={tableVisible} className={tableVisible ? 'buttons' : 'buttons1'} onClick={() => { settableVisible(true); fetchTablesStations(); setInfoMessage(''); setErrorMessage('') }}>List tables/stations</button>
+            <button disabled={!tableVisible} className={tableVisible ? 'buttons1' : 'buttons'} onClick={() => { settableVisible(false); setErrorMessage('') }}>Create new</button>
+        </div>
+        {tableVisible && (
+            <>
+                {errorMessage && (
+                    <div className="error-message">
+                        <img src={error_icon} alt='error' className='error-icon' />
+                        <span>{errorMessage}</span>
+                    </div>
+                )}
+                <div className='table'>
+                    <table border="1">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Table/Station number</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {stations.map(station => (
+                                <tr key={station.id}>
+                                    <td>{station.id}</td>
+                                    <td className="editable-cell">
+                                        {editingAdmin === admin ? (
+                                            <input id="numberEdit" type="text" defaultValue={station.number} className="editable-input" />
+                                        ) : (
+                                            station.number
+                                        )}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div> </>
+        )}
+    </div>
+
+    {!tableVisible && <div className='create'>
+        {infoMessage && (
+            <div className="info-message">
+                <img src={info_icon} alt='info' className='info-icon' />
+                <span>{infoMessage}</span>
+            </div>
+        )}
+        {errorMessage && (
+            <div className="error-message">
+                <img src={error_icon} alt='error' className='error-icon' />
+                <span>{errorMessage}</span>
+            </div>
+        )}
+        <div className='createFields'>
+            <label htmlFor="number" className='fields'>Table/Station number:</label>
+            <input type="number" id="numberCreate" className="number-input" placeholder="Table/Station number" onChange={() => {setInfoMessage('')}}/>
+        </div>
+        <button className='button2' onClick={createTablesStations}>Create</button>
+    </div>
+    }
+</>
+
+    
+);
+
+};
+
+export default CRUDTablesStations;
