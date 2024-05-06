6184fb384ca72e61282d94856367febf3db3407e
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2024-05-06 20:14:46 +0200

    added diagrams

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,22 +1,27 @@
-import Modal from 'react-modal';
-import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
-import Home from '../Home/Home';
-import useScript from '../../hooks/useScript';
-import importTableauScript from '../../hooks/useScript';
+import Modal from "react-modal";
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import Home from "../Home/Home";
+import "./Statistics.css";
+import useScript from "../../hooks/useScript";
 
-const Statistics = () => { 
+const Statistics = () => {
+  useScript(
+    "https://prod-uk-a.online.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js",
+    "https://prod-uk-a.online.tableau.com/t/nkrupalija1eeecd345e0/views/API/Sheet3"
+  );
 
-    useScript("https://prod-uk-a.online.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js","https://prod-uk-a.online.tableau.com/t/nkrupalija1eeecd345e0/views/API/Sheet2")
-   
-     
-    return (
-       <Home>
-        <>
-        </>
-       </Home>
-    )
+  return (
+    <Home>
+      <h2>Statistics</h2>
+      <div className="divWrapper" id="statisticsDiv">
+        <div className="divDiagramButtons">
+          <button className="diagramButton">Diagram 1</button>
+          <button className="diagramButton">Diagram 2</button>
+        </div>
+      </div>
+    </Home>
+  );
 };
-
 
 export default Statistics;
--- src/hooks/useScript.js
+++ src/hooks/useScript.js
@@ -1,40 +1,35 @@
 import { tab } from "@testing-library/user-event/dist/cjs/convenience/tab.js";
 import { useEffect } from "react";
 
+const useScript = (url, tableUrl) => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = url;
+    script.async = true;
+    script.type = "module";
 
-const useScript = (url, tableUrl) => {    
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = url;
-        script.async = true;
-        script.type = 'module';
+    const tableau = document.createElement("tableau-viz");
+    tableau.src = tableUrl;
+    tableau.async = true;
+    tableau.id = "tableau-viz";
+    tableau.toolbar = "bottom";
+    tableau.hidetabs = true;
 
-        
-        const tableau = document.createElement('tableau-viz');
-        tableau.src = tableUrl;
-        tableau.async = true;
-        tableau.id = "tableau-viz";
-        tableau.toolbar = "bottom";
-        tableau.hidetabs = true;   
-        
-       
+    tableau.style.height = "100%";
+    tableau.style.width = "100%";
+    tableau.style.margin = "0 auto";
+    tableau.style.display = "block";
 
-        tableau.style.width = "80%";
-       
+    const statisticsDiv = document.getElementById("statisticsDiv");
 
+    document.body.appendChild(script);
+    statisticsDiv.appendChild(tableau);
 
-      
-        document.body.appendChild(script);
-        document.body.appendChild(tableau);
-
-        return () => {
-            document.body.removeChild(script);
-            document.body.removeChild(tableau);
-        }
-    }, [url]);
-}
-
-
-
+    return () => {
+      document.body.removeChild(script);
+      statisticsDiv.removeChild(tableau);
+    };
+  }, [url]);
+};
 
 export default useScript;
